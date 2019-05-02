542e3ca2d6d33aa0b3c71dfd57bd6f5344e9eacc
Artem Kulakov <artem.kulakov@gmail.com>
2019-05-02 12:25:12 +0300

    Chart: all data at once

--- bitcoin/ViewController.swift
+++ bitcoin/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chtChart: LineChartView!
+    
+    var visitors: [Double] = [1.0, 1.5, 1.3, 1.4, 1.0, 2.1, 2.2, 1.5]
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scheduledTimerWithTimeInterval()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    func scheduledTimerWithTimeInterval(){
+        // Scheduling timer to Call the function "updateCounting" with the interval of 1 seconds
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateChart), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func updateChart() {
+        var chartEntry = [ChartDataEntry]()
+        
+        
+        
+        for i in 0..<visitors.count {
+            let value = ChartDataEntry(x: Double(i), y: visitors[i])
+            chartEntry.append(value)
+        }
+        
+        let line = LineChartDataSet(entries: chartEntry, label: "Visitor")
+        line.colors = [UIColor.green]
+        
+        let data = LineChartData()
+        data.addDataSet(line)
+        
+        chtChart.data = data
+        chtChart.chartDescription?.text = "Visitors Count"
+    }
 }
 
