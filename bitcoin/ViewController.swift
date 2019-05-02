040a002c91f1a6193ddbb097e8cb666e0191308c
Артем <artem.kulakov@gmail.com>
2019-05-02 11:23:07 +0300

    Initial Commit

---
+++ bitcoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitcoin
+//
+//  Created by Artem Kulakov on 02.05.2019.
+//  Copyright © 2019 Code Ultras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
